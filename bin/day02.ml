2da3d9e92f2e56af0d69af41e2702a21d7748769
Dayton <me@daytonforehand.dev>
2023-12-07 06:28:22 -0600

    beginnings of day 2 pt 1 🤔

--- bin/day02.ml
+++ bin/day02.ml
@@ -1,4 +1,30 @@
 open Core
 
+(* let is_game_valid str =
+  let draws = String.split ~on:';' str in
+
+let is_hand_valid str = 
+  let cube_sets = String.split ~on:',' str
+  |> List.map ~f:(fun substr ->
+      let trimmed = String.strip substr
+    ) *)
+
+type subset = { count : int; color : string; }
+
+let parse_subset str =
+  let trimmed = String.strip str in
+  List.find_map ["red"; "green"; "blue";] ~f:(fun color ->
+    match String.is_substring trimmed ~substring:color with
+    | false -> None
+    | true -> Some { count = (String.get trimmed 0 |> Char.to_int) - 48 ; color = color }
+  )
+  
+let is_subset_valid subset = 
+  match subset with
+  | { color = "red"; count; } -> count <= 12
+  | { color = "green"; count; } -> count <= 13
+  | { color = "blue"; count; } -> count <= 14
+  | _ -> print_endline "Invalid subset"; false
+
 let () = 
   print_endline "test"
